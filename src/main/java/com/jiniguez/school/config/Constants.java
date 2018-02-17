4d32e5daa699d7c413f3142b661906481b83bdb6
sozu00 <inhiguez.jesus@gmail.com>
2018-02-17 22:45:37 +0100

    sonar review

--- src/main/java/com/jiniguez/school/config/Constants.java
+++ src/main/java/com/jiniguez/school/config/Constants.java
@@ -3,7 +3,7 @@ package com.jiniguez.school.config;
 public final class Constants {
 
 	private Constants() {
-		throw new UnsupportedOperationException();
+		throw new IllegalStateException("Clase de utilidad no instanciable");
 	}
 	public static final Integer DEFAULT_PAGE = 0;
 	public static final Integer DEFAULT_SIZE = 10;
