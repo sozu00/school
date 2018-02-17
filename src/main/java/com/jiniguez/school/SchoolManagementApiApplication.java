3905a4ffa9827fe3c054b93827f1a79aec025463
sozu00 <inhiguez.jesus@gmail.com>
2018-02-17 10:48:00 +0100

    commit inicial

---
+++ src/main/java/com/jiniguez/school/SchoolManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.jiniguez.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jiniguez/school/SchoolManagementApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jiniguez.school;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolManagementApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
