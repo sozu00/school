9ab1ff1f0af40b2a53c24c747f8a73d464a97894
sozu00 <inhiguez.jesus@gmail.com>
2018-02-17 22:44:45 +0100

    sonar review

--- src/main/java/com/jiniguez/school/config/Constants.java
+++ src/main/java/com/jiniguez/school/config/Constants.java
@@ -2,7 +2,9 @@ package com.jiniguez.school.config;
 
 public final class Constants {
 
-	public Constants() {}
+	public Constants() {
+		throw new UnsupportedOperationException();
+	}
 	public static final Integer DEFAULT_PAGE = 0;
 	public static final Integer DEFAULT_SIZE = 10;
 }
